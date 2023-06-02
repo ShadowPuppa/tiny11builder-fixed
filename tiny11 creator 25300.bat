d4d256c3a957bda95366fa3fb7c32020fb6e0439
ShadowPuppa <135317417+ShadowPuppa@users.noreply.github.com>
2023-06-02 09:28:18 +0100

    fix to tiny11 creator 25300.bat
    Fixes crashing issue when running on some systems

--- tiny11 creator 25300.bat
+++ tiny11 creator 25300.bat
@@ -9,20 +9,6 @@ cls
 set DriveLetter=
 set /p DriveLetter=Please enter the drive letter for the Windows 11 image: 
 set "DriveLetter=%DriveLetter%:"
-echo.
-if not exist "%DriveLetter%\sources\boot.wim" (
-	echo.Can't find Windows OS Installation files in the specified Drive Letter..
-	echo.
-	echo.Please enter the correct DVD Drive Letter..
-	goto :Stop
-)
-
-if not exist "%DriveLetter%\sources\install.wim" (
-	echo.Can't find Windows OS Installation files in the specified Drive Letter..
-	echo.
-	echo.Please enter the correct DVD Drive Letter..
-	goto :Stop
-)
 md c:\tiny11
 echo Copying Windows image...
 xcopy.exe /E /I /H /R /Y /J %DriveLetter% c:\tiny11 >nul
